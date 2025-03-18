4cb28a0446e60bb83f04209e87a8fe3045377495
mjlee111 <menggu1234@naver.com>
2025-03-18 16:15:59 +0900

    dfadf

--- scripts/umi_script.sh
+++ scripts/umi_script.sh
@@ -6,10 +6,11 @@ if [ "$current_user" = "right" ]; then
     echo "Right User"
     cd /home/right/rpi_umi/py
     python umi_stream.py --host_ip 192.168.0.141 --port 11001 --id UMI_GRIPPER_RIGHT
-else
+elif [ "$current_user" = "left" ]; then
     echo "Left User"
     cd /home/left/rpi_umi/py
     python umi_stream.py --host_ip 192.168.0.141 --port 11002 --id UMI_GRIPPER_LEFT
+else
+    echo "Invalid User"
 fi
-
 echo "UMI Script End"
