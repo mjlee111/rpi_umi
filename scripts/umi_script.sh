0eef67507ed9fd179c4ebda15ea63da2f5512f35
mjlee111 <menggu1234@naver.com>
2025-03-18 16:44:49 +0900

    dd

--- scripts/umi_script.sh
+++ scripts/umi_script.sh
@@ -3,16 +3,18 @@ current_user=$(whoami)
 echo "UMI Script"
 echo "Current User: $current_user"
 
+target_ip = 192.168.0.141
+
 if [ "$current_user" = "right" ]; then
     echo "Right User"
     source /home/right/umi/bin/activate
     cd /home/right/rpi_umi/py
-    python umi_stream.py --host_ip 192.168.0.141 --port 11001 --id UMI_GRIPPER_RIGHT
+    python umi_stream.py --host_ip $target_ip --port 11001 --id UMI_GRIPPER_RIGHT
 elif [ "$current_user" = "left" ]; then
     echo "Left User"
     source /home/left/umi/bin/activate
     cd /home/left/rpi_umi/py
-    python umi_stream.py --host_ip 192.168.0.141 --port 11002 --id UMI_GRIPPER_LEFT
+    python umi_stream.py --host_ip $target_ip --port 11002 --id UMI_GRIPPER_LEFT
 else
     echo "Invalid User"
 fi
