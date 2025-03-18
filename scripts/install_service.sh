124cb09ce07b61fe122c185120d2ef9261bdcebb
mjlee111 <menggu1234@naver.com>
2025-03-18 16:39:53 +0900

    fin

--- scripts/install_service.sh
+++ scripts/install_service.sh
@@ -28,3 +28,24 @@ echo "You can check the status with: sudo systemctl status udpstream"
 echo "You can check the logs with: sudo journalctl -u udpstream -f"
 echo "To start/stop the service: sudo systemctl start/stop udpstream"
 echo "To restart the service: sudo systemctl restart udpstream"
+
+echo "alias udp_restart='sudo systemctl restart udpstream.service'" >> ~/.bashrc
+echo "alias udp_log='journalctl -u udpstream.service -f'" >> ~/.bashrc
+echo "alias udp_status='sudo systemctl status udpstream.service'" >> ~/.bashrc
+
+source ~/.bashrc
+
+echo "Alias udp_restart, udp_log, udp_status has been added to your bashrc"
+
+echo "You can now use the following commands to manage the service:"
+echo "  - sudo systemctl start/stop udpstream"
+echo "  - sudo systemctl restart udpstream"
+echo "  - sudo journalctl -u udpstream -f"
+echo "  - sudo systemctl status udpstream"
+
+echo "You can also use the following aliases:"
+echo "  - udp_restart"
+echo "  - udp_log"
+echo "  - udp_status"
+
+
