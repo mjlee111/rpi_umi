0e5546dfe27734f5894c923846291f634723daaf
mjlee111 <menggu1234@naver.com>
2025-03-18 15:44:18 +0900

    fix: command

--- umi_button.py
+++ umi_button.py
@@ -8,8 +8,6 @@ import os
 KEY6 = 26
 KEY8 = 21
 
-username = os.getlogin()
-
 button6 = Button(KEY6)
 button8 = Button(KEY8)
 
@@ -19,10 +17,10 @@ def handle_button_press(button, key_name):
     print(f"{key_name} PRESS")
     if key_name == "KEY6":
         print("Key Event : Exiting!!!!!!!!!") ## Ctrl+c
-        os.system("sudo kill -2 $(pgrep -f 'python3 /home/" + username + "/rpi_umi/umi_stream.py')")
+        os.system("sudo kill -2 $(pgrep -f 'python3 /home/right/rpi_umi/umi_stream.py')")
     elif key_name == "KEY8":
         print("Key Event : Starting!!!!!!!!!") ## START
-        os.system("sudo python3 /home/" + username + "/rpi_umi/umi_stream.py")
+        os.system("sudo python3 /home/right/rpi_umi/umi_stream.py")
         
 button6.when_pressed = lambda: handle_button_press(button6, "KEY6")
 button8.when_pressed = lambda: handle_button_press(button8, "KEY8")
