9449b1101a34be3fb32d6089270cc58c9589ec81
mjlee111 <menggu1234@naver.com>
2025-03-18 15:48:14 +0900

    freatafdadsfvav df

--- umi_button.py
+++ umi_button.py
@@ -1,32 +1,40 @@
 #!/usr/bin/python
 
-from gpiozero import Button
+import RPi.GPIO as GPIO
 from signal import pause, signal, SIGINT
 import sys
 import os
 
+# GPIO 설정
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
 KEY6 = 26
 KEY8 = 21
 
-button6 = Button(KEY6)
-button8 = Button(KEY8)
+# GPIO 핀 설정
+GPIO.setup(KEY6, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(KEY8, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 print("UMI Button Initialized")
 
-def handle_button_press(button, key_name):
-    print(f"{key_name} PRESS")
-    if key_name == "KEY6":
+def handle_button_press(channel):
+    if channel == KEY6:
+        print("KEY6 PRESS")
         print("Key Event : Exiting!!!!!!!!!") ## Ctrl+c
         os.system("sudo kill -2 $(pgrep -f 'python3 /home/right/rpi_umi/umi_stream.py')")
-    elif key_name == "KEY8":
+    elif channel == KEY8:
+        print("KEY8 PRESS")
         print("Key Event : Starting!!!!!!!!!") ## START
         os.system("sudo python3 /home/right/rpi_umi/umi_stream.py")
-        
-button6.when_pressed = lambda: handle_button_press(button6, "KEY6")
-button8.when_pressed = lambda: handle_button_press(button8, "KEY8")
+
+# 이벤트 핸들러 등록
+GPIO.add_event_detect(KEY6, GPIO.FALLING, callback=handle_button_press, bouncetime=300)
+GPIO.add_event_detect(KEY8, GPIO.FALLING, callback=handle_button_press, bouncetime=300)
 
 def signal_handler(sig, frame):
     print("\nProgram terminated by user")
+    GPIO.cleanup()
     sys.exit(0)
 
 # Register the signal handler
